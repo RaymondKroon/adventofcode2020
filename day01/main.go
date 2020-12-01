d049f2f2034bfee7cd25f0e4a29ec60d39657f43
Raymond Kroon <raymond.kroon@kadaster.nl>
2020-12-01 09:37:38 +0100

    day01

---
+++ day01/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "strconv"
+)
+
+
+
+func main() {
+    file, err := os.Open("./input/day01.txt")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)
+    var text []string
+
+    for scanner.Scan() {
+        text = append(text, scanner.Text())
+    }
+
+    var input []int
+    for _, s := range text {
+       i, _ := strconv.Atoi(s)
+       input = append(input, i)
+    }
+
+    for iIdx, i := range input {
+        for _, j := range input[iIdx+1:] {
+            if i + j == 2020 {
+                println(i*j)
+            }
+        }
+    }
+}
