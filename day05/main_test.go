5a4894cfe16fe038c96ff35f2343e7058a19024c
Raymond Kroon <raymond@k3n.nl>
2020-12-06 08:29:08 +0100

    day05 - benchmark (file)

---
+++ day05/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"adventofcode2020"
+	"testing"
+)
+
+func Benchmark(b *testing.B) {
+	benchmarks := []struct {
+		name string
+		fn   func(s string) BoardingPass
+	}{
+		{name: "calculate", fn: ParseBoardingPassOld},
+		{name: "bitshift", fn: ParseBoardingPass},
+	}
+	stringInput, _ := adventofcode2020.ReadInputLines("../input/day05.txt")
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				for _, input := range stringInput {
+					bm.fn(input)
+				}
+			}
+		})
+	}
+}
