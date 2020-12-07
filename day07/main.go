59f40fcb62934148ff0e3c77407a7ac4c1f6a267
Raymond Kroon <raymond.kroon@kadaster.nl>
2020-12-07 13:53:23 +0100

    day07 completed

--- day07/main.go
+++ day07/main.go
@@ -53,10 +53,67 @@ func ParseBagRules(input []string) []BagRule {
 
 }
 
+func findBagsWichContains(rules []BagRule, color string) []BagRule {
+	result := make([]BagRule, 0)
+	for _, r := range rules {
+		for _, b := range r.Bags {
+			if b.Color == color {
+				result = append(result, r)
+			}
+		}
+	}
+
+	return result
+}
+
+func searchUp(rules []BagRule, color string, selected *[]string) {
+	new := findBagsWichContains(rules, color)
+	for _, br := range new {
+		if !adventofcode2020.StringInSlice(br.Color, *selected) {
+			*selected = append(*selected, br.Color)
+			searchUp(rules, br.Color, selected)
+		}
+	}
+	return
+}
+
+func selectBagWithColor(rules []BagRule, color string) BagRule {
+	for _, br := range rules {
+		if br.Color == color {
+			return br
+		}
+	}
+	return BagRule{}
+}
+
+func searchDown(rules []BagRule, color string) int {
+	br := selectBagWithColor(rules, color)
+	result := 0
+	if len(br.Bags) > 0 {
+		for _, b := range br.Bags {
+			result += b.N * (1 + searchDown(rules, b.Color))
+		}
+	}
+	return result
+}
+
+func Part1CountShinyGoldParents(rules []BagRule) int {
+	total := make([]string, 0)
+	searchUp(rules, "shiny gold", &total)
+
+	return len(total)
+}
+
+func Part2CountShinyGoldChildren(rules []BagRule) int {
+	return searchDown(rules, "shiny gold")
+}
+
 func main() {
 	inputLines, _ := adventofcode2020.ReadInputLines("./input/day07.txt")
 	rules := ParseBagRules(inputLines)
-	for _, r := range rules {
-		fmt.Println(r)
-	}
+	answer1 := Part1CountShinyGoldParents(rules)
+	fmt.Printf("(part1) bags %d\n", answer1)
+
+	answer2 := Part2CountShinyGoldChildren(rules)
+	fmt.Printf("(part2) bags %d\n", answer2)
 }
