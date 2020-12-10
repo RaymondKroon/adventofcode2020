777d03a0e7f149748248d17cf53f1263bde45278
Raymond Kroon <raymond.kroon@kadaster.nl>
2020-12-10 11:52:07 +0100

    day 10

---
+++ day10/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"adventofcode2020"
+	"fmt"
+	"sort"
+)
+
+func Part1(input []int) int {
+	voltages := make([]int, len(input))
+	copy(voltages, input)
+	sort.Ints(voltages)
+	voltages = append([]int{0}, voltages...)
+	voltages = append(voltages, voltages[len(voltages)-1]+3)
+
+	diffs := make(map[int]int)
+	for i := 0; i < len(voltages)-1; i++ {
+		diffs[voltages[i+1]-voltages[i]] += 1
+	}
+
+	return diffs[1] * diffs[3]
+}
+
+func Part2(input []int) int {
+	voltages := make([]int, len(input))
+	copy(voltages, input)
+	sort.Ints(voltages)
+	voltages = append([]int{0}, voltages...)
+	nPaths := make(map[int]int)
+	nPaths[0] = 1
+	for i := 0; i < len(voltages); i++ {
+		v := voltages[i]
+		next := voltages[i:]
+		for _, step := range []int{1, 2, 3} {
+			if adventofcode2020.Contains(next, v) {
+				nPaths[v+step] += nPaths[v]
+			}
+		}
+	}
+
+	return nPaths[voltages[len(voltages)-1]]
+}
+
+func main() {
+	defer adventofcode2020.Stopwatch("Run")()
+	lines, _ := adventofcode2020.ReadInputLines("./input/day10.txt")
+	voltages, _ := adventofcode2020.Atoi(lines)
+
+	p1 := Part1(voltages)
+	fmt.Println("(part1)", p1)
+
+	p2 := Part2(voltages)
+	fmt.Println("(part2)", p2)
+}
--- shared.go
+++ shared.go
@@ -139,3 +139,12 @@ func Max(array []int) int {
 	}
 	return result
 }
+
+func Contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
